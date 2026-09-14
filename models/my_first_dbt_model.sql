5f1387898bc044b09004aa834763809c2edea3c6
CLAUDEVAN DOS SANTOS <cavalcanteclaudevan@gmail.com>
2026-09-14 03:26:16 +0000

    commit de sincronismo

--- models/my_first_dbt_model.sql
+++ models/my_first_dbt_model.sql
@@ -10,15 +10,15 @@ Try changing "table" to "view" below
 
 */
 
-config(materialized='table')
+{{ config(materialized='table') }}
 
 with source_data as (
 
-select 1 as id
+    select 1 as id
 
-union all
+    union all
 
-select null as id
+    select null as id
 
 )
 
@@ -33,5 +33,3 @@ Uncomment the line below to remove records with null id values
 */
 
 -- where id is not null
-
-
